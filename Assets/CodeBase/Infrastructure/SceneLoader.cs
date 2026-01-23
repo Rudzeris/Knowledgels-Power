ac6afc382e319e30807df42008f7910b04529e77
Rudzeris <ruzil1atangulov@gmail.com>
2026-01-24 03:32:06 +0500

    SceneLoader is correct now

--- Assets/CodeBase/Infrastructure/SceneLoader.cs
+++ Assets/CodeBase/Infrastructure/SceneLoader.cs
@@ -9,28 +9,31 @@ namespace CodeBase.Infrastructure
     {
         private readonly ICoroutineRunner _routineRunner;
 
-        public SceneLoader(ICoroutineRunner routineRunner) 
+        public SceneLoader(ICoroutineRunner routineRunner)
             => _routineRunner = routineRunner;
-        
-        public void Load(string sceneName, Action onLoaded =null)
+
+        public void Load(string sceneName, Action onLoaded = null)
             => _routineRunner.StartCoroutine(LoadScene(sceneName, onLoaded));
-        
+
         private IEnumerator LoadScene(string sceneName, Action onLoaded = null)
         {
-            if(sceneName == SceneManager.GetActiveScene().name)
+            if (sceneName == SceneManager.GetActiveScene().name)
+            {
+                onLoaded?.Invoke();
                 yield break;
-            
+            }
+
             var waitNextScene = SceneManager.LoadSceneAsync(sceneName);
-            
-            if(waitNextScene == null)
+
+            if (waitNextScene == null)
             {
                 Debug.LogError($"Scene '{sceneName}' not found");
                 yield break;
             }
-            
-            while(!waitNextScene.isDone)
+
+            while (!waitNextScene.isDone)
                 yield return null;
-            
+
             onLoaded?.Invoke();
         }
     }
